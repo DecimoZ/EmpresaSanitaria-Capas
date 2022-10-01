c1f20d81266b360f70c8b5d9699a871f5a162482
DecimoZ <113647054+DecimoZ@users.noreply.github.com>
2022-10-01 12:37:35 -0300

    Agregar archivos de proyecto.

---
+++ EmpresaSanitaria Capas/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaSanitaria_Capas
+{
+    public class Class1
+    {
+    }
+}
